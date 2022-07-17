e92a33280e0163e4de593674547d284bc3f5620b
judahsullivan <sullivan.judah95@gmail.com>
2022-07-17 14:35:29 -0500

    edit readme and full page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+<div class="bg-gradient-to-r from-cyan-500 to-blue-400 h-screen">
+    <NavigationMenu/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavigationMenu from "./Components/NavigationMenu.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: { NavigationMenu }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import url('https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@400;500;600;700&family=Rubik+Moonrocks&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+
 </style>
---
+++ src/Components/NavigationMenu.vue
@@ -0,0 +1,44 @@
+<template>
+<div className='fixed w-full text-white flex justify-between p-4 items-center'>
+
+        <div className='text-2xl font-bold text-center uppercase'>
+            <h1 class="text-bold text-black">J <span className='block text-4xl text-white'>Sullivan</span></h1>
+        </div>
+
+        <nav>
+
+            <div className='absolute right-6 md:hidden top-6 scale-150'>
+                
+            </div>
+
+                <ul className='hidden md:flex gap-8 p-6 uppercase bg-white/10 '>
+                    <li class="cursor-pointer  hover:text-black"><router-link to="/">Home</router-link></li>
+                    <li><router-link to="/projects">Projects</router-link></li>
+                    <li><router-link to="/services">Services</router-link></li>
+                    <li><router-link to="/reviews">Reviews</router-link></li>
+                    <li><router-link to="/contacts">Contact Me</router-link></li>
+                    
+
+                </ul>
+
+                
+        </nav>
+
+    </div>
+  
+</template>
+
+<script>
+
+
+
+
+
+ 
+</script>
+<style>
+.nav-item{
+  justify-content: space-between
+}
+
+</style>
